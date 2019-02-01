e0c63d8fcba5048fc3e8a5f16926c7b449dd227c
Priyesh <91priyesh@gmail.com>
2019-01-31 21:12:39 -0500

    added functions to and inquirer prompts taking in user input of what they want to purchase and finding it on mySQL database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -40,6 +40,66 @@ function connect() {
 const displayProductTable = () => {
   db.query("SELECT * FROM products", function (err, dbProducts) {
     if (err) throw err;
+
+    //display table of current items with inventory from mySQL
     console.table(dbProducts);
+
+    //inquirer prompt after displaying list of products to ask which item they would want to bid on.
+    inquirer.prompt([{
+      name: "purchaseQuestion",
+      message: "What item would you like to purchase?",
+      type: "input",
+      default: "(please type in item_id of product)",
+      validate: function (input) {
+        return !isNaN(input);
+      },
+      filter: function (input) {
+        return parseInt(input);
+      }
+    }, ]).then(({
+      purchaseQuestion,
+      quantityQuestion
+    }) => {
+      //linking item id to product in database
+      const itemId = dbProducts.find(item => item.item_id === purchaseQuestion);
+
+      console.log(itemId);
+      //prompt confriming product selection, if not loop back, if yes move to quantityquesiton
+      inquirer.prompt({
+        name: "confirmPrompt",
+        type: "list",
+        message: `You want to purchase ${itemId.product_name}?`,
+        choices: ["Yes", "No", "Exit"]
+      }).then(function (response) {
+        if (response.confirmPrompt === "Yes") {
+          quantityQuestion();
+        } else if (response.confirmPrompt === "No") {
+          console.log("Okay try lets try again! What would you like to buy select from the list");
+          displayProductTable();
+        } else if (response.confirmPrompt === "Exit") {
+          connection.end();
+        }
+      });
+
+      function quantityQuestion() {
+        
+      }
+
+    })
   })
 }
+
+
+
+// {
+//   name: "quantityQuestion",
+//   message: "What quantity?",
+//   type: "input",
+//   default: "please type in item_Id of product",
+//   validate: function (input) {
+//     return !isNaN(input);
+//   },
+//   filter: function (input) {
+//     return parseInt(input);
+//   }
+// }
